c0c99aa5c71eca4151f50004a070433183d0d757
HinataAoki <sunreverse.0311.blue@gmail.com>
2023-03-05 16:30:48 +0900

    update

---
+++ GenerativeInstancing/README.md
@@ -0,0 +1,1 @@
+https://youtu.be/wGXTokt-G0E
--- NotionPython/query.py
+++ NotionPython/query.py
@@ -6,7 +6,9 @@ DATABASE_ID = "YOUR_DATABASE_ID"
 NOTION_API_KEY = "YOUR_NOTION_API_KEY"
 
 today = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
-date = format(today, '%Y-%m-%d')
+afterweek = today + datetime.timedelta(weeks=1)
+today = format(today, '%Y-%m-%d')
+afterweek = format(afterweek, '%Y-%m-%d')
 
 url = "https://api.notion.com/v1/databases/" + DATABASE_ID + "/query"
 
@@ -18,16 +20,33 @@ headers = {
 
 # データ取得
 data = {
-    "filter": {
-        "property": "Due Date",
-        "date": {
-            "before": date,
-            # "equals": date,
-            # "after": date,
-        }
-    }
+    "filter": {"and":
+        [
+            {"and":[{
+                "property": "Due Date",
+                "date": {"after": today}
+            },
+            {
+                "property": "Due Date",
+                "date": {"before": afterweek}
+            }]},
+            {"or":[{
+                "property": "Status",
+                "select": {"equals": "To Do"}
+            },
+            {
+                "property": "Status",
+                "select": {"equals": "In Progress"}
+            }]}
+        ]}
 }
 
 response = requests.post(url, headers=headers, json=data)
 data = response.json()
-print(data)
+
+print('1週間後までの予定をお知らせします。\n')
+for info in reversed(data["results"]):
+    print('Title: ', info['properties']['Task']['title'][0]['plain_text'])
+    print('Status: ', info['properties']['Status']['select']['name'])
+    print('DueDate: ', info['properties']['Due Date']['date']['start'])
+    print('- - - - - - -')
